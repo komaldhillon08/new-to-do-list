957232135032f472dd31deccf474e55d0877548e
Sandeep <your-email@example.com>
2025-04-01 19:04:19 +0530

    add css anad js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,22 @@ inputText.addEventListener('keydown', function(event) {
         if (inputText.value.trim() === "") {
             alert("Type something");
         } else {
+            // add li 
             let li = document.createElement("li"); 
             li.textContent = inputText.value; 
             listContainer.appendChild(li);
+
+          // add input 
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox" ;  // Example text for the span
+            checkbox.classList = "checkbox" ;
+
+          // add cross img 
+          let img = createElement("img") ; 
+          img.src = "./assets/icon-cross.svg"
+
+
+           li.prepend(checkbox);
             inputText.value = ""; // Clear input after adding
         }
     }
