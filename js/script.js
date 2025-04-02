d0e323f60ba344b34413fe1576642351f984e64a
Sandeep <your-email@example.com>
2025-04-02 11:03:25 +0530

    add css anad js

--- js/script.js
+++ js/script.js
@@ -3,27 +3,31 @@ console.log("komal");
 let inputText = document.getElementById("input-text");
 let listContainer = document.getElementsByClassName("list-container")[0]; // First list container
 
-inputText.addEventListener('keydown', function(event) {
+inputText.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         if (inputText.value.trim() === "") {
             alert("Type something");
         } else {
             // add li 
-            let li = document.createElement("li"); 
-            li.textContent = inputText.value; 
+            let li = document.createElement("li");
+            li.textContent = inputText.value;
             listContainer.appendChild(li);
 
-          // add input 
+            // add input 
             let checkbox = document.createElement("input");
-            checkbox.type = "checkbox" ;  // Example text for the span
-            checkbox.classList = "checkbox" ;
+            checkbox.type = "checkbox";  // Example text for the span
+            checkbox.classList = "checkbox";
 
-          // add cross img 
-          let img = createElement("img") ; 
-          img.src = "./assets/icon-cross.svg"
+            // add cross img 
+            let img = document.createElement("img");
+            img.src = "./assets/icon-cross.svg"
+            img.classList.add("cross");
 
 
-           li.prepend(checkbox);
+
+            li.prepend(checkbox);
+            li.appendChild(img);
+
             inputText.value = ""; // Clear input after adding
         }
     }
