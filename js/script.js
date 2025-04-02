c943313c91f42afb74a013dac3ce238901d2dfdd
Sandeep <your-email@example.com>
2025-04-02 11:44:49 +0530

    add the cross and work the cross

--- js/script.js
+++ js/script.js
@@ -11,24 +11,50 @@ inputText.addEventListener('keydown', function (event) {
             // add li 
             let li = document.createElement("li");
             li.textContent = inputText.value;
-            listContainer.appendChild(li);
 
             // add input 
             let checkbox = document.createElement("input");
             checkbox.type = "checkbox";  // Example text for the span
             checkbox.classList = "checkbox";
 
+            checkbox.addEventListener("click", function (event) {
+                // console.log(event.target);
+                if (checkbox.checked) {
+                    li.style.textDecoration = "line-through";
+
+                } else {
+                    li.style.textDecoration = "none";
+                }
+
+            })
+
+
+
             // add cross img 
             let img = document.createElement("img");
             img.src = "./assets/icon-cross.svg"
             img.classList.add("cross");
 
+            img.addEventListener("click", function (event) {
+                // console.log(event.target);
+                let li = event.target.closest("li");
+                if (li) {
+                    li.remove();
+                    console.log("itme - remove");
+
+                } else {
+
+                }
+
+            })
 
 
+            listContainer.appendChild(li);
             li.prepend(checkbox);
             li.appendChild(img);
-
             inputText.value = ""; // Clear input after adding
+
+
         }
     }
 });
